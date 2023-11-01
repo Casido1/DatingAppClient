b929f5d04532bc52c3e65cf7b8250171060b1cf7
Casido1 <ugochukwu.anunihu@gmail.com>
2023-11-01 23:47:11 +0100

    Converted the register form to a reactive form and added input validations

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,20 +11,29 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  registerForm: FormGroup | undefined
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService){}
 
   ngOnInit(): void {
- 
+    this.initializeForm();
   }
 
   initializeForm(){
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl(),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValue('password')]),
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     })
+  }
+
+  matchValue(matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register(){
